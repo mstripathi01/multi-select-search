cf9953748f96e73cbb8a1e47eda51093a4394f2a
mstripathi01 <mayank.tripathi218@gmail.com>
2024-01-30 16:35:33 +0530

    multi-select-search

---
+++ src/components/pills.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+const Pills = ({ image, text, onClick }) => {
+  return (
+    <span className="user-pill" onClick={onClick}>
+      <img src={image} alt={text} />
+      <span>{text} &times;</span>
+    </span>
+  );
+};
+
+export default Pills;
